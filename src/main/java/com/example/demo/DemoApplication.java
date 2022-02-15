900d416a5558063f632756f795d1bc1525892c81
unknown <rafa.rotsy@gmail.com>
2022-02-15 19:24:23 +0300

    Revert "websercurity"
    This reverts commit 1d291f6c36484e5395ee0182768d9c9427a69ad2.

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -22,17 +22,5 @@ public class DemoApplication {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 	
-	 @Bean
-	    public WebMvcConfigurer corsConfigurer() {
-	        return new WebMvcConfigurer() {
-	            @Override
-	            public void addCorsMappings(CorsRegistry registry) {
-	                registry.addMapping("/**")
-	                        .allowedMethods("HEAD", "GET", "POST", "PUT", "DELETE", "OPTIONS")
-	                        .allowedHeaders("*").allowedOrigins("*");
-	            }
-	        };
-	 }
-     
 
 }
