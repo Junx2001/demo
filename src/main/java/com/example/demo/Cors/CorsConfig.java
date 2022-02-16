37b97c92d1ba346c052f405816be4a789067864e
unknown <rafa.rotsy@gmail.com>
2022-02-16 13:57:40 +0300

    Update CorsConfig.java

--- src/main/java/com/example/demo/Cors/CorsConfig.java
+++ src/main/java/com/example/demo/Cors/CorsConfig.java
@@ -1,14 +1,22 @@
 package com.example.demo.Cors;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@EnableWebMvc
 public class CorsConfig implements WebMvcConfigurer {
-    @Override
+ 
+	@Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
+        		.allowedOrigins("*")
+        		.allowedHeaders("*")
                 .allowedMethods("*");
     }
+	
 }
