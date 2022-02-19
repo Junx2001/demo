fc1ab6fbbf113c64c322ca7941253069cf623d45
unknown <rafa.rotsy@gmail.com>
2022-02-19 19:30:44 +0300

    Update EnableCors.java

--- src/main/java/com/example/demo/cors/EnableCors.java
+++ src/main/java/com/example/demo/cors/EnableCors.java
@@ -1,18 +1,21 @@
 package com.example.demo.cors;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-public class EnableCors {
-	@Bean
-    public WebMvcConfigurer configurer(){
-      return new WebMvcConfigurer(){
-        @Override
-        public void addCorsMappings(CorsRegistry registry) {
-          registry.addMapping("/*")
-              .allowedOrigins("*");
-        }
-      };
-	}
+@Configuration
+@EnableWebMvc
+public class EnableCors implements WebMvcConfigurer {
+	 @Override
+	    public void addCorsMappings(CorsRegistry registry) {
+	        registry.addMapping("/**")  // allow cros path
+	        .allowedOrigins("*")    // allow cros origin
+	        .allowedMethods("POST", "GET", "PUT", "OPTIONS", "DELETE")// allow method
+	        .maxAge(168000) //
+	        .allowedHeaders("*")
+	        .allowCredentials(true);
+	    }
 }
