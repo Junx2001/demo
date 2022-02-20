536a5bc5716a316becd2c652529f06805312e367
unknown <rafa.rotsy@gmail.com>
2022-02-20 21:20:45 +0300

    j

--- src/main/java/com/example/demo/filter/FilterFront.java
+++ src/main/java/com/example/demo/filter/FilterFront.java
@@ -43,11 +43,6 @@ public class FilterFront implements Filter {
 
         HttpServletRequest req = (HttpServletRequest) request;
         HttpServletResponse res = (HttpServletResponse) response;
-        res.setHeader("Access-Control-Allow-Origin", "*");
-        res.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
-        res.setHeader("Access-Control-Max-Age", "3600");
-        res.setHeader("Access-Control-Allow-Headers", "x-requested-with, authorization, content-type");
-
        
         String bearerToken = req.getHeader("Authorization");
         
--- src/main/java/com/example/demo/filter/SimpleCorsFilter.java
+++ src/main/java/com/example/demo/filter/SimpleCorsFilter.java
@@ -21,7 +21,30 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
+@Component
 public class SimpleCorsFilter {
-	
+
+	    @Bean
+	    public FilterRegistrationBean<CorsFilter> initCorsFilter() {
+	    	List<String> origins = new ArrayList<String>();
+	    	origins.add("http://localhost:4200/**");
+	    	origins.add("https://spring-joharisoa.herokuapp.com/**");
+	    	origins.add("https://signgovfo.herokuapp.com/**");
+	    	origins.add("http://spring-joharisoa.herokuapp.com/**");
+	    	origins.add("http://signgovfo.herokuapp.com/**");
+	        // @formatter:off
+	        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+	        CorsConfiguration config = new CorsConfiguration();
+	        config.setAllowCredentials(true);
+	        config.setAllowedHeaders(Arrays.asList("Authorization", "Cache-Control", "Content-Type"));
+	        config.addAllowedMethod("*");
+	        config.setAllowedOrigins(origins);
+	        source.registerCorsConfiguration("/**", config);
+	        FilterRegistrationBean<CorsFilter> bean = new FilterRegistrationBean<>(new CorsFilter(source));
+	        bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+	        System.out.println("TTTTTTTTTTSSSSSSSS");
+	        return bean;
+	        // @formatter:on
+	    }
 
 }
