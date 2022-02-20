94ff2423342fea94f888afa843ac032d69696acd
unknown <rafa.rotsy@gmail.com>
2022-02-20 21:14:46 +0300

    Update SimpleCorsFilter.java

--- src/main/java/com/example/demo/filter/SimpleCorsFilter.java
+++ src/main/java/com/example/demo/filter/SimpleCorsFilter.java
@@ -21,7 +21,6 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
-@Component
 public class SimpleCorsFilter {
 	
 
